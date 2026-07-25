817edd6f62ceb0593c9609c33b9bde296d54d308
Jon Jaques <jaquers@gmail.com>
2026-07-24 20:09:51 -0500

    Initial Commit

---
+++ Synesthia/SynesthiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynesthiaApp.swift
+//  Synesthia
+//
+//  Created by Jon Jaques on 7/24/26.
+//
+
+import SwiftUI
+
+@main
+struct SynesthiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
